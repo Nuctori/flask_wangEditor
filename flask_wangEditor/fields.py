8862af8509a329525a71ce28aed1d697ff795964
Nuctori <1018570975@qq.com>
2020-03-21 19:27:40 +0800

    fix autoSave bug

--- flask_wangEditor/fields.py
+++ flask_wangEditor/fields.py
@@ -7,7 +7,7 @@
     :copyright: (c) 2020 by Nuctori.
     :license: MIT, see LICENSE for more details.
 """
-from wtforms import TextAreaField
+from wtforms import StringField
 
 from wtforms.widgets.core import HTMLString,html_params,escape
 from wtforms.compat import text_type
@@ -21,11 +21,11 @@ class WangEditor():
         if 'required' not in kwargs and 'required' in getattr(field, 'flags', []):
             kwargs['required'] = True
         editor_id = field.name + '_editor'
-        return HTMLString('<div id={id} ></div><input hidden=true %s>%s</input>'.format(id=editor_id) % ( 
+        return HTMLString('<div id={id}></div><input hidden=true %s value="%s"></input>'.format(id=editor_id) % ( 
             html_params(name=field.name, **kwargs),
             escape(text_type(field._value()), quote=False)
         )) # div is the editor, input will get the content from div by onchange callback in js code
 
 
-class WangEditorField(TextAreaField):
+class WangEditorField(StringField):
     widget = WangEditor()
